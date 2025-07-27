46eed7a63c5b3814fa0dc87b9ce33d92eddf54f7
SociopathEliot <dlaonlcahik@gmail.com>
2025-07-27 16:46:01 +0600

    Add maps activity with location and image

--- app/src/main/java/be/buithg/homework5/MainActivity.kt
+++ app/src/main/java/be/buithg/homework5/MainActivity.kt
@@ -1,10 +1,13 @@
 package be.buithg.homework5
 
 import android.os.Bundle
+import android.content.Intent
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val imageView: ImageView = findViewById(R.id.imageView)
+        Glide.with(this)
+            .load("https://picsum.photos/400")
+            .into(imageView)
+
+        imageView.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
         }
     }
 }
